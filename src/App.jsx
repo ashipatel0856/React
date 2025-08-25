e5c628aed6c48dd476ce264c0339770ec83dfb8a
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-08-26 00:45:40 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Movies from './MovieZone/Movies'
 import UseEffect from './components/UseEffect'
+import Fetch_Data_API from './components/Fetch_Data_API'
+import Form from './components/Form'
+import Multiple_Input_Handling from './components/Multiple_Input_Handling'
 const App = () => {
 
   return (
    <>
    {/* <Movies /> */}
-   <UseEffect />
+  {/* <Fetch_Data_API /> */}
+  {/* <Form /> */}
+  <Multiple_Input_Handling />
    </>
-  )
-}
+  );
+};
 
 export default App;
