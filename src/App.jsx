92fb88a6f6f43193fd838d660e46e798d4269be1
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-08-26 19:23:46 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import UseEffect from './components/UseEffect'
 import Fetch_Data_API from './components/Fetch_Data_API'
 import Form from './components/Form'
 import Multiple_Input_Handling from './components/Multiple_Input_Handling'
+import Meal from './Food_Recipe/Meal'
 const App = () => {
 
   return (
@@ -10,7 +11,8 @@ const App = () => {
    {/* <Movies /> */}
   {/* <Fetch_Data_API /> */}
   {/* <Form /> */}
-  <Multiple_Input_Handling />
+  {/* <Multiple_Input_Handling /> */}
+  <Meal />
    </>
   );
 };
