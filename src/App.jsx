b157d6208a0714de84c87ec68e769d186b534eda
Ashish Kumar <ashishkumarr0856@gmail.com>
2025-08-27 20:28:04 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,18 @@ import Fetch_Data_API from './components/Fetch_Data_API'
 import Form from './components/Form'
 import Multiple_Input_Handling from './components/Multiple_Input_Handling'
 import Meal from './Food_Recipe/Meal'
+
+
+
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Team from './pages/Team'
+import Product from './pages/Product'
+import Course_Detail from './pages/Course_Details'
+import { Navbar } from './components/Navbar'
+
 const App = () => {
 
   return (
@@ -12,7 +24,22 @@ const App = () => {
   {/* <Fetch_Data_API /> */}
   {/* <Form /> */}
   {/* <Multiple_Input_Handling /> */}
-  <Meal />
+  {/* <Meal /> */}
+
+
+  <Router>
+    < Navbar />
+    <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/>
+        <Route path="/team" element={<Team />}/>
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/courses" element={<Product />}/>
+       <Route path="/courses/:id" element={<Course_Detail />}/>
+
+
+    </Routes>
+  </Router>
    </>
   );
 };
